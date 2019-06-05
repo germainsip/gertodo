7aa8c2b9c88d387e79ec4ca7b9c1e24cdcd576a7
Germain SIPIERE <gsipiere@gmail.com>
2019-06-05 09:40:42 +0200

    quelques modification

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <v-toolbar app>
+        <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="text-uppercase">
+        <span class="font-weight-light">Todo</span>
+        <span>de Germain</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon href="https://github.com/germainsip" target="_blank">
+        <v-icon>fab fa-github</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app>test</v-navigation-drawer>
+  </nav>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            drawer: false
+        }
+    },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container grid-list-md>
+      <span>
+        <v-img :src="require('@/assets/logoG.png')" contain height="150"></v-img>
+      </span>
+      <h1 class="display-2">
+        Salut je suis une app 
+        <span class="text-uppercase light-green--text">  electron </span>
+      </h1>
+      
+      
+      <v-btn block dark color="indigo">Commencer<v-icon right>fas fa-door-open</v-icon></v-btn>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {}
+};
 </script>
