9e298eac17e9e0b75ca7db824e2f66a44c548524
Germain SIPIERE <gsipiere@gmail.com>
2019-06-08 15:24:11 +0200

    Ajout de projets

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="dashboard">
     <v-container class="my-5">
- <span>
-        <v-img :src="require('@/assets/logoG.png')" contain height="170" class="mb-4"></v-img>
-      </span>
-      <h1 class="display-2">
-        Salut je suis une app 
-        <span class="text-uppercase light-green--text">  electron </span>
-      </h1>
-      
-      
-      <v-btn block dark color="indigo">Commencer<v-icon right>fas fa-door-open</v-icon></v-btn>
-   
+      <v-card  class="pa-3 ma-1">
+        <span>
+          <v-img :src="require('@/assets/logoG.png')" contain height="170" class="mb-4"></v-img>
+        </span>
+        <h1 class="display-2">
+          Salut je suis une app
+          <span class="text-uppercase light-green--text">electron</span>
+          
+        </h1>
+<p class="subheading">adaptée sur un Tuto de "The Net Ninja"</p>
+        <v-btn block dark color="indigo">
+          Commencer
+          <v-icon right>fas fa-door-open</v-icon>
+        </v-btn>
+      </v-card>
     </v-container>
-     
   </div>
 </template>
 
